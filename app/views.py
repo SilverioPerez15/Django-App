8a69f00e5dafe93e44bed357fb200abe92721abc
SILVERIO <juansilverio99@gmail.com>
2021-01-13 04:44:43 -0600

    version 1

---
+++ app/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Producto
+from .forms import ContactoForm, ProductoForm, CustomUserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
+
+def home(request):
+    productos = Producto.objects.all()
+    data = {
+        'productos': productos
+    }
+    return render(request, 'app/home.html', data)
+
+
+def contacto(request):
+    data = {
+        'form': ContactoForm()
+    }
+
+    if request.method == 'POST':
+        formulario = ContactoForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"] = "Contacto guardado"
+        else:
+            data["form"] = formulario
+
+    return render(request, 'app/contacto.html', data)
+
+
+def agregar_producto(request):
+    data = {
+        'form': ProductoForm()
+    }
+
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Producto registrado")
+        else:
+            data["form"] = formulario
+
+    return render(request, 'app/producto/agregar.html', data)
+
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    data = {
+        'productos': productos
+    }
+
+    return render(request, 'app/producto/listar.html', data)
+
+
+def modificar_producto(request, id):
+
+    producto = get_object_or_404(Producto, id=id)
+    data = {
+        'form': ProductoForm(instance=producto)
+    }
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, instance=producto, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Modificado correctamente")
+            return redirect(to="listar_productos")
+        data["form"] = formulario
+    return render(request, 'app/producto/modificar.html', data)
+
+
+def eliminar_producto (request,id):
+    producto = get_object_or_404(Producto, id=id)
+    producto.delete()
+    messages.success(request, "Eliminado correctamente")
+    return redirect(to=listar_productos)
+
+
+def registro(request):
+    data = {
+        'form': CustomUserCreationForm()
+    }
+
+    if request.method == 'POST':
+        formulario = CustomUserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            user = authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
+            login(request, user)
+            messages.success(request, "El usuario ha sido registrado con exito")
+            return redirect(to="home")
+        data["form"] = formulario
+
+    return render(request, 'registration/registro.html', data)
